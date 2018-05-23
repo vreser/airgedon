7411b0d17d1f86ccf4e1a9dda04e89cc3c04cf18
IwraStudios <IwraStudios@users.noreply.github.com>
2018-05-23 17:29:06 +0200

    Some changes to the detection (output works)
    Change the string to WPA, "handshake" does not show on small screens
    Add sleep before killing airodump-ng so it has extra time to detect changes

--- airgeddon.sh
+++ airgeddon.sh
@@ -8301,8 +8301,9 @@ function attack_handshake_menu() {
 	debug_print
 
 	if [ "${1}" = "handshake" ]; then
+		sleep 4s
 		kill "${processidcapture}" &> /dev/null
-		if grep -q handshake "handcheck.txt"; then
+		if grep -q WPA "handcheck.txt"; then
 
 			handshakepath="${default_save_path}"
 			lastcharhandshakepath=${handshakepath: -1}
