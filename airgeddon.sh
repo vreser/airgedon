3d850b1cf0f8d2e72e96eae7e79c3f11782f70c2
v1s1t0r1sh3r3 <v1s1t0r.1s.h3r3@gmail.com>
2019-02-13 00:18:50 +0100

    Preparing version numbers for 9.10

--- airgeddon.sh
+++ airgeddon.sh
@@ -2,8 +2,8 @@
 #Title........: airgeddon.sh
 #Description..: This is a multi-use bash script for Linux systems to audit wireless networks.
 #Author.......: v1s1t0r
-#Date.........: 20190131
-#Version......: 9.01
+#Date.........: 20190213
+#Version......: 9.10
 #Usage........: bash airgeddon.sh
 #Bash Version.: 4.2 or later
 
@@ -109,8 +109,8 @@ declare -A possible_alias_names=(
 								)
 
 #General vars
-airgeddon_version="9.01"
-language_strings_expected_version="9.01-1"
+airgeddon_version="9.10"
+language_strings_expected_version="9.10-1"
 standardhandshake_filename="handshake-01.cap"
 timeout_capture_handshake="20"
 tmpdir="/tmp/"
